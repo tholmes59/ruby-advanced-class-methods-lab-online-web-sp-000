97d6c806811d9a1add04b7d1ad6ec84b3a1f5f18
Tom Holmes <tholmes59@gmail.com>
2019-01-25 00:08:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == name
+    if self.find_by_name(name)
      self.find_by_name(name)
   else
     self.create_by_name(name)
